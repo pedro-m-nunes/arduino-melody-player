736847bd9420ff9ae912467821061913388749a3
Pedro Nunes <pedro.familianunes@gmail.com>
2023-12-28 16:40:14 -0300

    Organizing files

--- playMelody.ino
+++ playMelody.ino
@@ -4,7 +4,7 @@
 
 const int piezoPin = 11;
 
-int notes[] = {
+int notes[] = { // Can't Stop by the Red Hot Chili Peppers (Bass riff)
   E2, D3, E3, E2, PAUSE, E2, D3, PAUSE, E3, PAUSE, E2, 
   D2, D3, E3, D2, PAUSE, D2, D3, PAUSE, E3, PAUSE, A2, 
   B1, B2, B2, B1, B1, B2, B1, B2, PAUSE, B1, 
@@ -28,7 +28,7 @@ void setup() {
   
   for (int i = 0; i < notesArraySize; i++) {
     double noteLength = barDuration / noteLengthDenominators[i];
-    // pauseBetweenNotes
+    // pauseBetweenNotes // ?
     
     tone(piezoPin, notes[i], noteLength * (1 - pauseDurationPercentage));
     delay(noteLength);
@@ -36,4 +36,6 @@ void setup() {
   }
 }
 
-void loop() {}
+void loop() {
+  
+}
